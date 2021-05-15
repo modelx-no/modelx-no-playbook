84febb69da05448a0dee99a286ec2ea120beb5a6
Erik Hagen <erik.hag1@gmail.com>
2021-05-15 23:54:34 +0200

    Dev

--- update.sh
+++ update.sh
@@ -20,7 +20,8 @@ echo ""
 echo ""
 echo "Running Antora..."
 echo "================="
-antora --fetch antora-playbook.yml --attribute lang=no 	--attribute wysiwig_editing=0  --attribute toc=! --attribute sectnums=!
+#antora --fetch antora-playbook.yml --attribute lang=no 	--attribute wysiwig_editing=0  --attribute toc=! --attribute sectnums=!
+antora --fetch antora-playbook.yml --attribute lang=no 	--attribute wysiwig_editing=0  --attribute toc=!
 echo ""
 echo "Updating site..."
 echo "================"
